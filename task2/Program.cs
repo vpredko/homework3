0d80b2715ed79ad4bb097c5b2e5b6c6c0cf08c5f
Vika <vpredko@mail.ru>
2022-07-03 17:41:27 +0300

    Solve task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите координаты x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Sqrt ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2)));
