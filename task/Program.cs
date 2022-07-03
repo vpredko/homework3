017f2ab1cef590f2ed62e0c9f4b0df7e8d9871a1
Vika <vpredko@mail.ru>
2022-07-03 17:26:27 +0300

    Solve task1

---
+++ task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a1 = number % 10;
+int a2 = number/10 % 10;
+int a3 = number/100 % 10;
+int a4 = number/1000 % 10;
+int a5 = number/10000;
+int num = a1*10000 + a2*1000 + a3*100 + a4*10 + a5;
+if (num == number)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
